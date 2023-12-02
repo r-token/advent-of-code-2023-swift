c8d845c2cf2228ac0ff521e3bafe6f2ad4fd2d5d
Ryan Token <ryan@storyboard.fm>
2023-12-01 17:58:44 -0700

    Fix build errors

--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -25,12 +25,12 @@ final class Day01Tests: XCTestCase {
     """
 
     func testPart1() throws {
-        let challenge = Day00(data: testData)
+        let challenge = Day01(data: testData)
         XCTAssertEqual(String(describing: challenge.part1()), "6000")
     }
 
     func testPart2() throws {
-        let challenge = Day00(data: testData)
+        let challenge = Day01(data: testData)
         XCTAssertEqual(String(describing: challenge.part2()), "32000")
     }
 }
